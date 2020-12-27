bcecdaf66a5d7847e4dac50173b4dfb1383dad0e
PROinALL <vadimgangsta73@gmail.com>
2020-12-27 15:05:57 +0300

    fixed screen ratio, added simple zooming with mouse wheel
    also changed colors.
    This is literally my first contribution on GitHub so don't judge hard.

--- examples/tree.rs
+++ examples/tree.rs
@@ -1,12 +1,12 @@
-use macroquad::prelude::*;
+use macroquad::{Window, prelude::*};
 
 fn tree(gl: &mut QuadGl, time: f64, deep: u32, angle: f32, tall: f32) {
-    if deep >= 8 {
+    if deep >= 10 {
         return;
     }
 
     // we can use normal macroquad drawing API here
-    draw_rectangle(-0.01 / 2., 0., 0.01, tall, RED);
+    draw_rectangle(-0.01 / 2., 0., 0.01, tall, GREEN);
 
     // and we can also modify internal macroquad render state with "gl" reference
     gl.push_model_matrix(glam::Mat4::from_translation(glam::vec3(0., tall, 0.)));
@@ -26,19 +26,34 @@ fn tree(gl: &mut QuadGl, time: f64, deep: u32, angle: f32, tall: f32) {
     gl.pop_model_matrix();
 }
 
+//I'm not sure but it seems to me that it should be in some helpers of Macroquad
+fn clamp(x: f32, low: f32, high: f32)->f32{
+    match x{
+        _ if x<low => low,
+        _ if x>high => high,
+        _ => x
+    }
+}
+
 #[macroquad::main("Tree")]
 async fn main() {
-    let camera = Camera2D {
-        zoom: vec2(1., 1.),
-        target: vec2(0.0, 0.5),
-        ..Default::default()
-    };
+    let mut scale = 0.7; //good on 1920x1080 when fullscreen
 
-    set_camera(camera);
     loop {
-        clear_background(GREEN);
+        let camera = Camera2D {
+            zoom: vec2(1.*scale, scale*screen_width()/screen_height()), //fixed screen proportions
+            target: vec2(0.0, 0.5),
+            ..Default::default()
+        };
 
-        draw_circle(0., 0., 0.03, RED);
+        set_camera(camera);
+        scale*= 1.0 + (mouse_wheel().1 as f32)/10.0; //modifying scale
+        scale = clamp(scale, 0.02, 5.0); //making it not to go too far or too close
+        //println!("{}", scale); //debug
+
+        clear_background(BLUE);
+        
+        //draw_circle(0., 0., 0.03, GREEN); //initial position marker
         tree(unsafe { get_internal_gl().quad_gl }, get_time(), 0, 1., 0.3);
 
         next_frame().await
